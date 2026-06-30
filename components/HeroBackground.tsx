1bf4169f9bc6ed0c6f90b9de538b9c1264a76782
kaushal9393 <kaushalgiri8080@gmail.com>
2026-06-30 16:37:58 +0530

    fix: update useRef for React 19 types and sync tsconfig

--- components/HeroBackground.tsx
+++ components/HeroBackground.tsx
@@ -6,7 +6,7 @@ import { Points, PointMaterial } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 function StarBackground(props: any) {
-  const ref = useRef<any>();
+  const ref = useRef<any>(null);
   // 5001 is a multiple of 3 (1667 points * 3 coordinates) to prevent NaN errors in THREE.js
   const [sphere] = useState(() => random.inSphere(new Float32Array(5001), { radius: 1.5 }));
 
